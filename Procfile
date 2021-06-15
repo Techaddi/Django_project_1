de0f40787bc939adbe36b2f29dfb32f737b5c2ad
Techaddi <ankitsir90@gmail.com>
2021-06-15 11:48:24 +0530

    upto date

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guicorn second.wsgi --log file-
--- second/settings.py
+++ second/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hyvy@x(i+@km3%%jwa^@ew*dzd%x&)h*+*y9^dlp!$t!gzxf5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
